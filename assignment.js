eb8cfbd15db2d7062052ee81c10d4cd6faf71c1b
unknown <iarafat386@gmail.com>
2021-01-22 16:52:10 +0600

    Problem-01 Solved

---
+++ assignment.js
@@ -0,0 +1,16 @@
+//github repo link here
+
+// Problem-01(Kilometer to Meter Converter)
+function kilometerToMeter(kiloMeter) {
+    if (kiloMeter >= 0) {
+        let result = kiloMeter * 1000;
+        return result;
+    }
+    else {
+        return "Please input a valid number."
+    }
+    return result;
+}
+
+// console.log(kilometerToMeter(11.4));
+
